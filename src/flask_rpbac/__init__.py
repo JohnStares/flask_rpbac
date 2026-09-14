2746aa0a86ce879816281965cd2fb37b7c023858
JohnStares <wisdom8achor24@gmail.com>
2026-09-14 14:06:09 +0100

    refactor: Cache config information
    - Included default values to host, port and db.
    - Removed decode_responses as dev has no control of the output.
    - Also added username, password and ping_on_init values to the config.

--- src/flask_rpbac/__init__.py
+++ src/flask_rpbac/__init__.py
@@ -548,11 +548,12 @@ class RPBAC:
             config = CacheConfig(
                 type=_config.get("type"),
                 url=_config.get("url"),
-                host=_config.get("host"),
-                port=_config.get("port"),
-                db=_config.get("db"),
+                host=_config.get("host", "localhost"),
+                port=_config.get("port", 6379),
+                db=_config.get("db", 0),
+                username=_config.get("username"),
                 password=_config.get("password"),
-                decode_responses=_config.get("decode_responses"),
+                ping_on_init=_config.get("ping_on_init", True),
             )
 
             config_fac = CacheFactory(config)
